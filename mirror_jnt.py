8575317a6b98d1d989d00abc09179cca20b76421
Zeplaro <robinservoz@gmail.com>
2017-02-15 19:13:43 +0100

    Create mirror_jnt.py

---
+++ mirror_jnt.py
@@ -0,0 +1,17 @@
+#Mirror JNT & orient L>R
+
+import maya.cmds as mc
+
+sel=mc.ls(sl=1)
+
+for each in sel:
+    right=each.replace('L_','R_',1)
+    mc.duplicate(each,n=right)
+    mc.parent(right,w=1)
+    mc.setAttr(right+'.translateX',mc.getAttr(right+'.translateX')*-1)
+    X=mc.getAttr(right+'.jointOrientX')
+    Y=mc.getAttr(right+'.jointOrientY')
+    Z=mc.getAttr(right+'.jointOrientZ')
+    mc.setAttr(right+'.jointOrientX',X)
+    mc.setAttr(right+'.jointOrientY',Y*-1)
+    mc.setAttr(right+'.jointOrientZ',Z*-1)
