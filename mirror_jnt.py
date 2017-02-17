87f4e3db49c2c58bd7859a468f786194f8990ddf
Zeplaro <robinservoz@gmail.com>
2017-02-17 11:26:07 +0100

    Update mirror_jnt.py

--- mirror_jnt.py
+++ mirror_jnt.py
@@ -15,3 +15,6 @@ for each in sel:
     mc.setAttr(right+'.jointOrientX',X)
     mc.setAttr(right+'.jointOrientY',Y*-1)
     mc.setAttr(right+'.jointOrientZ',Z*-1)
+    
+    if mc.listRelatives(each,ap=1):
+        mc.parent(right,mc.listRelatives(each,ap=1)[0].replace('L_','R_',1))
