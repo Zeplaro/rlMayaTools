027d326747aff21e6f957144d5635f578641a012
Robin Lavigne <robinservoz@gmail.com>
2017-03-20 02:05:07 +0100

    def do

---
+++ rig/grpOrig.py
@@ -0,0 +1,19 @@
+import maya.cmds as mc
+def do_grpOrig():
+    sel=mc.ls(sl=1,typ='transform')
+    if sel:
+        for obj in sel:
+            grp=obj+'_orig'
+            if mc.objExists(grp):
+                mc.warning('Group orig already exits')
+            else:
+                grp=mc.group(em=1,n=grp)
+                mc.xform(grp,t=mc.xform(obj,q=1,ws=1,t=1),ro=mc.xform(obj,q=1,ws=1,ro=1),ws=1)
+                if mc.listRelatives(obj,ap=1):
+                    mc.parent(grp,mc.listRelatives(obj,ap=1))
+                    mc.parent(obj,grp)
+                else:
+                    mc.parent(obj,grp)
+        mc.select(sel,r=1)
+    else:
+        mc.warning('Please select an object')
