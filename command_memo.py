cd129b88ead929e884042f9ec29aa8217ef1ee2f
laro <robinservoz@gmail.com>
2017-03-24 18:15:51 +0100

    save

--- command_memo.py
+++ command_memo.py
@@ -5,4 +5,4 @@ om.MGlobal.displayInfo("my grey info message")
 #To append a python script path
 #It can be written in a userSetup.py file in your script folder to be load when maya starts
 import sys
-sys.path.append(r'D:\....\Work\Python\rlMayaTools')
+sys.path.append(r'/datas/laro/rlMayaTools/rig/')
--- rig/wip/shapeMirror.py
+++ rig/wip/shapeMirror.py
@@ -1,7 +1,18 @@
 import maya.cmds as mc
 
 
-def do_shapeMirror():
+def do_shapeMirror(axis='x',space='ws'):
+    if axis=='z':
+        axis=2
+    elif axis=='y':
+        axis=1
+    else:
+        axis=0
+    if space=='os':
+        space='os'
+    else:
+        space='ws'
+
     for ctrl in mc.ls(sl=1):
         nspace=ctrl.split(':')
         name=nspace[-1]
@@ -18,23 +29,47 @@ def do_shapeMirror():
             master=ctrl
             slave=nspace+name.replace('R_','L_',1)
         else:
+            mc.warning('No side found on '+ctrl+'!')
             continue
         if not mc.objExists(slave):
+            mc.warning(slave+' not found')
             continue
 
-        if mc.objectType(ctrl, isType='nurbsCurve') or mc.objectType(ctrl, isType='nurbsSurface'):
-            shM=[master]
-            shS=[slave]
-        elif mc.objectType(ctrl, isType='transform'):
-            shM=mc.listRelatives(master,c=1,s=1,pa=1)
-            shS=mc.listRelatives(slave,c=1,s=1,pa=1)
+        if mc.objectType(ctrl, isType='transform') or mc.objectType(ctrl, isType='joint'):
+            master=mc.listRelatives(master,c=1,s=1,pa=1)
+            slave=mc.listRelatives(slave,c=1,s=1,pa=1)
+        elif mc.objectType(ctrl, isType='nurbsCurve') or mc.objectType(ctrl, isType='nurbsSurface'):
+            master=[master]
+            slave=[slave]
         else:
+            mc.warning('No shape found in '+ctrl)
             continue
-        if (shM is None) or (shS is None):
+        if master is None:
+            mc.warning('No shape found in master '+master)
             continue
+        elif slave is None:
+            mc.warning('No shape found in slave '+slave)
+            continue
+        while len(master)>len(slave):
+            master.pop(-1)
 
-        for shape in shM:
-            cvs = mc.getAttr(shape+'.cp', s=1)
+        i=0
+        for shape in master:
+            altshape=slave[i]
+            i+=1
+            cvs=mc.getAttr(shape+'.cp', s=1)
+            altcvs=mc.getAttr(altshape+'.cp', s=1)
             for cv in range(cvs):
-                mc.move(0.1,0.1,0.1,shape+'.cp['+str(cv)+']',r=1)
+                cp='.cp['+str(cv)+']'
+                if space=='ws':
+                    pos=mc.xform(shape+cp,q=1,ws=1,t=1)
+                    pos[axis]*=-1
+                    mc.xform(altshape+cp,ws=1,t=pos)
+
+                #WIP
+                elif space=='os':
+                    pos=mc.xform(shape+cp,q=1,os=1,t=1)
+                    pos[axis]*=-1
+                    mc.xform(altshape+cp,os=1,t=pos)
+
     print('__DONE__')
