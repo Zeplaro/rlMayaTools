56975153909185c1f5d029d60a554454ac26ec62
Robin Lavigne <robinservoz@gmail.com>
2017-03-27 02:41:28 +0200

    update

--- rig/wip/shapeMirror.py
+++ rig/wip/shapeMirror.py
@@ -1,19 +1,29 @@
 import maya.cmds as mc
+import rig.getAxis as ga
 
+def do_shapeMirror(mirAxis='x', space='ws'):
+    '''
+    Mirror objects shape on defined axis in world or object space
+    :param str mirAxis: world axis on wich you want to mirror 'x'(default), 'y', 'z'
+    :param str space: 'ws'(default) mirror on world space, 'os' mirror on object space
+    '''
 
-def do_shapeMirror(axis='x',space='ws'):
-    if axis=='z':
-        axis=2
-    elif axis=='y':
-        axis=1
+    #defining index of mirAxis to mirror on choosen axis
+    if mirAxis== 'z':
+        mirAxis=2
+    elif mirAxis== 'y':
+        mirAxis=1
     else:
-        axis=0
+        mirAxis=0
+
+    #check what space was choosen
     if space=='os':
         space='os'
     else:
         space='ws'
 
     for ctrl in mc.ls(sl=1):
+        #checking for namespaces
         nspace=ctrl.split(':')
         name=nspace[-1]
         if len(nspace)>1:
@@ -22,6 +32,8 @@ def do_shapeMirror(axis='x',space='ws'):
             nspace+=':'
         else:
             nspace=''
+
+        #checking for side mark
         if name.startswith('L_'):
             master=ctrl
             slave=nspace+name.replace('L_','R_',1)
@@ -35,6 +47,18 @@ def do_shapeMirror(axis='x',space='ws'):
             mc.warning(slave+' not found')
             continue
 
+        #getting ctrls axis in case of object space
+        masterSpace = ga.do_getAxis(master, space='os')
+        slaveSpace = ga.do_getAxis(slave, space='os')
+        table=[1,1,1]
+        for i in range(3):
+            if not masterSpace[i] == slaveSpace[i]:
+                table[i]=-1
+        table[mirAxis]*=-1 #inverting axis on wich mirAxis is defined
+        print(table)
+
+
+        #Checking if the selection is valid
         if mc.objectType(ctrl, isType='transform') or mc.objectType(ctrl, isType='joint'):
             master=mc.listRelatives(master,c=1,s=1,pa=1)
             slave=mc.listRelatives(slave,c=1,s=1,pa=1)
@@ -44,11 +68,12 @@ def do_shapeMirror(axis='x',space='ws'):
         else:
             mc.warning('No shape found in '+ctrl)
             continue
+        #check if a shape as been found in selection
         if master is None:
-            mc.warning('No shape found in master '+master)
+            mc.warning('No shape found in master '+ctrl)
             continue
         elif slave is None:
-            mc.warning('No shape found in slave '+slave)
+            mc.warning('No shape found in slave '+ctrl)
             continue
         while len(master)>len(slave):
             master.pop(-1)
@@ -63,13 +88,14 @@ def do_shapeMirror(axis='x',space='ws'):
                 cp='.cp['+str(cv)+']'
                 if space=='ws':
                     pos=mc.xform(shape+cp,q=1,ws=1,t=1)
-                    pos[axis]*=-1
+                    pos[mirAxis]*=-1
                     mc.xform(altshape+cp,ws=1,t=pos)
 
                 #WIP
                 elif space=='os':
                     pos=mc.xform(shape+cp,q=1,os=1,t=1)
-                    pos[axis]*=-1
+                    for i in range(3):
+                        pos[i]=pos[i]*table[i]
                     mc.xform(altshape+cp,os=1,t=pos)
 
     print('__DONE__')
