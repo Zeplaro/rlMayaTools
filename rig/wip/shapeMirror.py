997b9a70e0ccc2d34d6c141870f8c3f6adae21cb
laro <robinservoz@gmail.com>
2017-03-27 19:24:39 +0200

    update

--- rig/wip/shapeMirror.py
+++ rig/wip/shapeMirror.py
@@ -1,11 +1,13 @@
 import maya.cmds as mc
-import rig.getAxis as ga
+import getAxis as ga
 
-def do_shapeMirror(mirAxis='x', space='ws'):
+def do_shapeMirror(mirAxis='x', os=False):
     '''
     Mirror objects shape on defined axis in world or object space
     :param str mirAxis: world axis on wich you want to mirror 'x'(default), 'y', 'z'
-    :param str space: 'ws'(default) mirror on world space, 'os' mirror on object space
+    __WIP__:param bool os: False(default) mirror on world space, True mirror on object space
+    
+    TODO: pop cluster shape in shape list
     '''
 
     #defining index of mirAxis to mirror on choosen axis
@@ -16,13 +18,8 @@ def do_shapeMirror(mirAxis='x', space='ws'):
     else:
         mirAxis=0
 
-    #check what space was choosen
-    if space=='os':
-        space='os'
-    else:
-        space='ws'
-
-    for ctrl in mc.ls(sl=1):
+    ctrls=mc.ls(sl=1)
+    for ctrl in ctrls:
         #checking for namespaces
         nspace=ctrl.split(':')
         name=nspace[-1]
@@ -41,61 +38,50 @@ def do_shapeMirror(mirAxis='x', space='ws'):
             master=ctrl
             slave=nspace+name.replace('R_','L_',1)
         else:
-            mc.warning('No side found on '+ctrl+'!')
             continue
         if not mc.objExists(slave):
-            mc.warning(slave+' not found')
             continue
 
+        #______WIP_____
         #getting ctrls axis in case of object space
-        masterSpace = ga.do_getAxis(master, space='os')
-        slaveSpace = ga.do_getAxis(slave, space='os')
-        table=[1,1,1]
-        for i in range(3):
-            if not masterSpace[i] == slaveSpace[i]:
-                table[i]=-1
-        table[mirAxis]*=-1 #inverting axis on wich mirAxis is defined
-        print(table)
-
+        loc = mc.spaceLocator(n='_loc')[0]
+        mc.parent(loc,slave,r=1)
+        mc.parent(loc,master)
+        table=ga.do_getAxis(loc,os=True)
+        mc.delete(loc)
+        table[mirAxis]*=-1 #mirroring axis on wich mirAxis is defined
 
         #Checking if the selection is valid
         if mc.objectType(ctrl, isType='transform') or mc.objectType(ctrl, isType='joint'):
-            master=mc.listRelatives(master,c=1,s=1,pa=1)
-            slave=mc.listRelatives(slave,c=1,s=1,pa=1)
-        elif mc.objectType(ctrl, isType='nurbsCurve') or mc.objectType(ctrl, isType='nurbsSurface'):
+            master=mc.listRelatives(master,c=1,s=1,pa=1, type='nurbsCurve') or []
+            slave=mc.listRelatives(slave,c=1,s=1,pa=1, type='nurbsCurve') or []
+        elif mc.objectType(ctrl, isType='nurbsCurve'):
             master=[master]
             slave=[slave]
         else:
-            mc.warning('No shape found in '+ctrl)
             continue
-        #check if a shape as been found in selection
-        if master is None:
-            mc.warning('No shape found in master '+ctrl)
-            continue
-        elif slave is None:
-            mc.warning('No shape found in slave '+ctrl)
-            continue
+
+        #checking nbr of shapes in master and slave
         while len(master)>len(slave):
             master.pop(-1)
 
         i=0
         for shape in master:
-            altshape=slave[i]
-            i+=1
             cvs=mc.getAttr(shape+'.cp', s=1)
-            altcvs=mc.getAttr(altshape+'.cp', s=1)
+            altcvs=mc.getAttr(slave[i]+'.cp', s=1)
             for cv in range(cvs):
                 cp='.cp['+str(cv)+']'
-                if space=='ws':
+                if not os:
                     pos=mc.xform(shape+cp,q=1,ws=1,t=1)
                     pos[mirAxis]*=-1
-                    mc.xform(altshape+cp,ws=1,t=pos)
+                    mc.xform(slave[i]+cp,ws=1,t=pos)
 
-                #WIP
-                elif space=='os':
+                #________WIP_________
+                else:
                     pos=mc.xform(shape+cp,q=1,os=1,t=1)
                     for i in range(3):
                         pos[i]=pos[i]*table[i]
-                    mc.xform(altshape+cp,os=1,t=pos)
-
+                    mc.xform(slave[i]+cp,os=1,t=pos)
+            i+=1
+    mc.select(ctrls,r=1)
     print('__DONE__')
