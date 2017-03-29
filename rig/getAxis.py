af021a3eb9ece9f9a7277c21699600ad898e53e0
Robin Lavigne <robinservoz@gmail.com>
2017-03-30 01:49:19 +0200

    update

--- rig/getAxis.py
+++ rig/getAxis.py
@@ -7,7 +7,8 @@ def do_getAxis(obj, os=False, exact=False):
     Find objects axis direction compared to world or parent axis
     :param str obj: object to query
     :param bool os: False(default) query direction compared to world, True query direction compared to parent
-    :return: axis direction in a list [1,1,1]
+    :param bool exact: True return exact direction value for each axis
+    :return: axis direction in a list, e.g.:[1, 1, 1]
     """
 
     vp = mc.createNode('vectorProduct', n='_vp')
@@ -22,10 +23,10 @@ def do_getAxis(obj, os=False, exact=False):
     fullval = []
     for i in range(3):
         j = 0
-        for dir in xyz:
-            mc.setAttr(vp+'.input1'+dir,val[j])
+        for dir in 'XYZ':
+            mc.setAttr(vp+'.input1'+dir, val[j])
             j += 1
-        for dir in xyz:
+        for dir in 'XYZ':
             fullval.append(mc.getAttr(vp+'.output'+dir))
         axis[i] = mc.getAttr(vp+'.output'+xyz[i])
         if axis[i] < 0:
@@ -36,27 +37,45 @@ def do_getAxis(obj, os=False, exact=False):
     fullval = [fullval[:3]]+[fullval[3:6]]+[fullval[6:]]
     mc.delete(vp)
     if exact:
-        return axis,fullval
+        return axis, fullval
     else:
         return axis
 
 
-def getMirrorTable(master, slave):
+def getMirrorTable (master, slave, miraxis='x'):
+    """
+    Return a mirror table between two object on chosen axis
+    :param str master: object to compare to slave
+    :param str slave: object to compare to master
+    :param str miraxis: 'x'(default) chosen world axis on wich mirror is wanted
+    :return: list: return a mirror table list, e.g.:[-1, 1, 1]
+    """
 
-    M = getAxisDir(master)
-    S = getAxisDir(slave)
-    mirtable = [1,1,1]
+    m = getAxisDir(master)
+    s = getAxisDir(slave)
+    mirtable = [1, 1, 1]
     for i in range(3):
-        if M[i][-1] == S[i][-1]:
-            if not M[i][0] == S[i][0]:
+        if m[i][-1] == s[i][-1]:
+            if not m[i][0] == s[i][0]:
                 mirtable[i] = -1
+            if m[i][-1] == miraxis:
+                mirtable[i] *= -1
         else:
             mirtable[i] = 0
     return mirtable
 
-def getAxisDir(obj):
+
+def getAxisDir (obj):
+    """
+    Return a list of axis direction compared to world
+    :param str obj: object on wich to check the axis
+    :return: list: return an axis direction list compared to world,
+                    first index giving the x axis direction and so on e.g.:[y, -z, -x],
+                    for a object matching world axis it will return : [x, y, z]
+    """
+
     objdir = do_getAxis(obj, exact=True)[-1]
-    face = []
+    dir = []
     for i in range(3):
         val = []
         for j in range(3):
@@ -64,24 +83,23 @@ def getAxisDir(obj):
         if abs(val[0]) > abs(val[1]):
             if abs(val[0]) > abs(val[2]):
                 if val[0] < 0:
-                    face.append('-x')
+                    dir.append('-x')
                 else:
-                    face.append('x')
+                    dir.append('x')
             else:
                 if val[2] < 0:
-                    face.append('-z')
+                    dir.append('-z')
                 else:
-                    face.append('z')
+                    dir.append('z')
         else:
             if abs(val[1]) > abs(val[2]):
                 if val[1] < 0:
-                    face.append('-y')
+                    dir.append('-y')
                 else:
-                    face.append('y')
+                    dir.append('y')
             else:
                 if val[2] < 0:
-                    face.append('-z')
+                    dir.append('-z')
                 else:
-                    face.append('z')
-    return face
-
+                    dir.append('z')
+    return dir
--- rig/wip/shapeMirror.py
+++ rig/wip/shapeMirror.py
@@ -1,85 +1,78 @@
 import maya.cmds as mc
 import rig.getAxis as ga
 
-def do_shapeMirror(mirAxis='x', os=False):
-    '''
+
+def do_shapeMirror(miraxis='x', ws=False):
+    """"
     Mirror objects shape on defined axis in world or object space
-    :param str mirAxis: world axis on wich you want to mirror 'x'(default), 'y', 'z'
-    __WIP__:param bool os: False(default) mirror on world space, True mirror on object space
-    
-    TODO: pop cluster shape in shape list
-    '''
+    :param str miraxis: world axis on wich you want to mirror 'x'(default), 'y', 'z'
+    :param bool ws: False(default) mirror on object space, True mirror on world space
+    """
 
-    #defining index of mirAxis to mirror on choosen axis
-    if mirAxis== 'z':
-        mirAxis=2
-    elif mirAxis== 'y':
-        mirAxis=1
+    # defining index of mirAxis to mirror on chosen axis
+    if miraxis == 'z':
+        mirindex = 2
+    elif miraxis == 'y':
+        mirindex = 1
     else:
-        mirAxis=0
+        mirindex = 0
 
-    ctrls=mc.ls(sl=1)
+    ctrls = mc.ls(sl=1)
     for ctrl in ctrls:
-        #checking for namespaces
-        nspace=ctrl.split(':')
-        name=nspace[-1]
-        if len(nspace)>1:
+        # checking for namespaces
+        nspace = ctrl.split(':')
+        name = nspace[-1]
+        if len(nspace) > 1:
             nspace.pop(-1)
-            nspace=':'.join(nspace)
-            nspace+=':'
+            nspace = ':'.join(nspace)
+            nspace += ':'
         else:
-            nspace=''
+            nspace = ''
 
-        #checking for side mark
+        # checking for side mark
         if name.startswith('L_'):
-            master=ctrl
-            slave=nspace+name.replace('L_','R_',1)
+            master = ctrl
+            slave = nspace+name.replace('L_', 'R_', 1)
         elif name.startswith('R_'):
-            master=ctrl
-            slave=nspace+name.replace('R_','L_',1)
+            master = ctrl
+            slave = nspace+name.replace('R_', 'L_', 1)
         else:
             continue
         if not mc.objExists(slave):
             continue
 
-        #______WIP_____
-        #getting ctrls axis in case of object space
-        table=ga.getMirrorTable(master,slave)
-        print(table)
-        table[mirAxis]*=-1
-        print(table)
+        # getting ctrls axis for object space
+        table = ga.getMirrorTable(master, slave, miraxis=miraxis)
 
-        #Checking if the selection is valid
+        # Checking if the selection is valid
         if mc.objectType(ctrl, isType='transform') or mc.objectType(ctrl, isType='joint'):
-            master=mc.listRelatives(master,c=1,s=1,pa=1, type='nurbsCurve') or []
-            slave=mc.listRelatives(slave,c=1,s=1,pa=1, type='nurbsCurve') or []
+            master = mc.listRelatives(master, c=1, s=1, pa=1, type='nurbsCurve') or []
+            slave = mc.listRelatives(slave, c=1, s=1, pa=1, type='nurbsCurve') or []
         elif mc.objectType(ctrl, isType='nurbsCurve'):
-            master=[master]
-            slave=[slave]
+            master = [master]
+            slave = [slave]
         else:
             continue
 
-        #checking nbr of shapes in master and slave
-        while len(master)>len(slave):
+        # checking nbr of shapes in master and slave
+        while len(master) > len(slave):
             master.pop(-1)
 
-        i=0
+        i = 0
         for shape in master:
-            cvs=mc.getAttr(shape+'.cp', s=1)
-            altcvs=mc.getAttr(slave[i]+'.cp', s=1)
+            cvs = mc.getAttr(shape+'.cp', s=1)
             for cv in range(cvs):
-                cp='.cp['+str(cv)+']'
-                if not os:
-                    pos=mc.xform(shape+cp,q=1,ws=1,t=1)
-                    pos[mirAxis]*=-1
-                    mc.xform(slave[i]+cp,ws=1,t=pos)
+                cp = '.cp['+str(cv)+']'
+                if ws:
+                    pos = mc.xform(shape+cp, q=1, ws=1, t=1)
+                    pos[mirindex] *= -1
+                    mc.xform(slave[i]+cp, ws=1, t=pos)
 
-                #________WIP_________
                 else:
-                    pos=mc.xform(shape+cp,q=1,os=1,t=1)
+                    pos = mc.xform(shape+cp, q=1, os=1, t=1)
                     for k in range(3):
-                        pos[k]=pos[k]*table[k]
-                    mc.xform(slave[i]+cp,os=1,t=pos)
-            i+=1
-    mc.select(ctrls,r=1)
+                        pos[k] = pos[k]*table[k]
+                    mc.xform(slave[i]+cp, os=1, t=pos)
+            i += 1
+    mc.select(ctrls, r=1)
     print('__DONE__')
