0817d659aec77666481b021aec385d8c3438d917
Robin Lavigne <robinservoz@gmail.com>
2017-04-04 00:50:06 +0200

    update

--- rig/selShape.py
+++ rig/selShape.py
@@ -1,7 +1,4 @@
 import maya.cmds as mc
-'''
-To do : check if already shape
-'''
 
 
 def do_selShape(q=False, objs=[]):
@@ -13,9 +10,16 @@ def do_selShape(q=False, objs=[]):
         objs = mc.ls(sl=1)
 
     for obj in objs:
-        child = mc.listRelatives(obj, c=1, typ='shape')
+        if mc.objectType(obj, isa='shape'):
+            childs += [obj]
+            continue
+        child = mc.listRelatives(obj, c=1, typ='shape') or []
         child = [shape for shape in child if 'Orig' not in shape]
         childs += child
+
+    shapes = []
+    [shapes.append(i) for i in childs if i not in shapes]
+
     if not q:
-        mc.select(childs,r=1)
-    return childs
+        mc.select(shapes,r=1)
+    return shapes
