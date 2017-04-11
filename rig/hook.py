4902984350dd86b0f7ff229ede1a7ac0f2ba506a
laro <robinservoz@gmail.com>
2017-04-11 19:13:12 +0200

    add objs arg

--- rig/hook.py
+++ rig/hook.py
@@ -1,16 +1,22 @@
 import maya.cmds as mc
 
 
-def do_hook():
+def do_hook(objs=None):
     hooks = []
-    if not mc.ls(sl=1, tr=1) == []:
-        targets = mc.ls(sl=1, tr=1)
-        for target in targets:
-            hook = mc.group(em=1, n=target + '_hook')
+    if objs:
+        if isinstance(objs, str) or isinstance(objs, unicode):  # if a string is given converts it in a list
+            objs = [objs]
+    else:
+        objs = mc.ls(sl=1, tr=1)
+    if objs:
+        for obj in objs:
+            if not mc.objExists(obj):
+                continue
+            hook = mc.group(em=1, n=obj + '_hook')
             hooks += [hook]
-            mmx = mc.createNode('multMatrix', n='mmx_' + target)
-            dmx = mc.createNode('decomposeMatrix', n='dmx_' + target)
-            mc.connectAttr(target + '.worldMatrix[0]', mmx + '.matrixIn[1]', f=1)
+            mmx = mc.createNode('multMatrix', n='mmx_' + obj)
+            dmx = mc.createNode('decomposeMatrix', n='dmx_' + obj)
+            mc.connectAttr(obj + '.worldMatrix[0]', mmx + '.matrixIn[1]', f=1)
             mc.connectAttr(hook + '.parentInverseMatrix[0]', mmx + '.matrixIn[2]', f=1)
             mc.connectAttr(mmx + '.matrixSum', dmx + '.inputMatrix', f=1)
 
