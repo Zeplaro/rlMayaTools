242e6b521028adeeab6d95cf69ceffe4775ac4f9
Robin Lavigne <robinservoz@gmail.com>
2017-04-20 01:00:21 +0200

    update vtx conversion from any type

--- rig/avLoc.py
+++ rig/avLoc.py
@@ -3,29 +3,15 @@ import maya.cmds as mc
 
 def do_avLoc():
 
-    vtxs = [x for x in mc.ls(sl=True, fl=True) if '.vtx[' in x] or []
-    ess = [x for x in mc.ls(sl=True, fl=True) if '.e[' in x] or []
-    fs = [x for x in mc.ls(sl=True, fl=True) if '.f[' in x] or []
-    if not ess and not vtxs and not fs:
-        mc.warning('Please select vertexes, edges or faces')
-        return 'Please select vertexes, edges or faces'
+    sel = mc.ls(sl=True, fl=True)
+    vtxs = mc.ls(mc.polyListComponentConversion(sel, tv=True), fl=True)
+    if not vtxs:
+        mc.warning('Select components or meshes and try again')
+        return
 
     pos = []
-    # Vertexes
     for vtx in vtxs:
-        pos.append(mc.xform(vtx, q=1, ws=1, t=1))
-    # Edges
-    for es in ess:
-        esvtxs = mc.xform(es, q=1, ws=1, t=1)
-        pos.append(esvtxs[0:3])
-        pos.append(esvtxs[3:6])
-    # Faces
-    for f in fs:
-        esvtxs = mc.xform(f, q=1, ws=1, t=1)
-        pos.append(esvtxs[0:3])
-        pos.append(esvtxs[3:6])
-        pos.append(esvtxs[6:9])
-        pos.append(esvtxs[9:12])
+        pos.append(mc.xform(vtx, q=True, ws=True, t=True))
 
     posx = []
     posy = []
@@ -36,5 +22,6 @@ def do_avLoc():
         posz.append(each[2])
     av = [(min(posx)+max(posx))/2, (min(posy)+max(posy))/2, (min(posz)+max(posz))/2]
     loc = mc.spaceLocator(n='avLoc#')
-    mc.xform(loc, ws=1, t=av)
+    mc.xform(loc, ws=True, t=av)
+    mc.select(sel, r=True)
     return loc
