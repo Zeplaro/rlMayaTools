c6e4f754b55299e42e6182fb3edb2c0a08089e92
laro <robinservoz@gmail.com>
2017-06-01 12:36:44 +0200

    del cls

--- rig/wip/cls/utilsbis.py
+++
@@ -1,7 +0,0 @@
-import maya.cmds as mc
-
-
-class utils:
-    def __init__(self, obj, shape):
-
-        self.shape = [shape for shape in mc.listRelatives(obj, s=True, pa=1) or [] if 'Orig' not in shape]
