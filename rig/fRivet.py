5e15f5f23ecfcaf6ea581ede7314f0061ab3468c
Robin Lavigne <robinservoz@gmail.com>
2017-07-13 01:57:50 +0200

    update

--- rig/fRivet.py
+++ rig/fRivet.py
@@ -1,43 +1,31 @@
 import maya.cmds as mc
-import follicle as f
 from tbx import getShape
 
 # todo : contiguous edges sorter or step by step edges selection interface
 
-nbOfColumn = 2
-width = 440
-
+########################################################################################################################
+nbOfColumn = 0
+width = 30
 
 def ui():
 
-    # TODO : ranger en collumn au lieu de row
 
     winID = 'rvtUI'
     if mc.window(winID, exists=True):
         mc.deleteUI(winID)
-    mc.window(winID, title='Follicle Rivet creator', s=False, rtf=True)
+    mc.window(winID, title='Follicle Rivet creator', w=width, s=True, rtf=True)
 
     mc.columnLayout('columnMain', w=width)
-    mc.rowLayout('rowTitle', nc=3)
-    mc.separator(style='none', w=21)
-    mc.text(label='Edges', align='center', w=200)
-    mc.text(label='Edges', align='center', w=200)
-    mc.setParent('..')
     mc.rowLayout('rowEdges', nc=100)
-    mc.columnLayout('columnAddDel')
+    mc.columnLayout('columnAddDel', cal='center')
     mc.button('+', w=20, command=addColumn)
     mc.button('-', w=20, command=delColumn)
     mc.setParent('..')
-    mc.columnLayout('columnEdges1')
-    mc.textScrollList('edges1', w=200, h=100, allowMultiSelection=True)
-    mc.button('upSel1', label='Update selection', w=200, command=lambda x: upEdges(1))
-    mc.setParent('..')
-    mc.columnLayout('columnEdges2')
-    mc.textScrollList('edges2', w=200, h=100, allowMultiSelection=True)
-    mc.button('upSel2', label='Update selection', w=200, command=lambda x: upEdges(2))
+    addColumn()
+    addColumn()
     mc.setParent('..')
     mc.setParent('..')
-    mc.button('Create fRivet', command=createFRivet)
+    mc.button('Create fRivet', command=createFRivet, w=width)
     mc.showWindow()
 
 
@@ -54,7 +42,8 @@ def addColumn(*args):
     global width
     width += 204
     nbr = str(nbOfColumn)
-    mc.columnLayout('columnEdges'+nbr, parent='rowEdges')
+    mc.columnLayout('columnEdges'+nbr, cal='center', parent='rowEdges')
+    mc.text(label='Edges', align='center', w=200)
     mc.textScrollList('edges'+nbr, w=200, h=100, allowMultiSelection=True)
     mc.button('upSel'+nbr, label='Update selection', w=200, command=lambda x: upEdges(nbr))
     mc.columnLayout('columnMain', e=True, w=width)
@@ -68,47 +57,53 @@ def delColumn(*args):
         mc.deleteUI('columnEdges'+nbr)
         nbOfColumn -= 1
         width -= 204
-        mc.columnLayout('columnMain', e=True, w=width)
+        mc.window('rvtUI', e=True, w=width)
 
 
 def createFRivet(*args):
     edges = []
     for i in range(nbOfColumn):
         edges.append(mc.textScrollList('edges'+str(i+1), q=True, ai=True))
-        print edges
+    do_fRivet(*edges)
+########################################################################################################################
 
 
-def do_fRivet(*args):
+def do_fRivet(edges, mesh=None, nb=1):
     """
-    :param args: if more than two edges, input edges (number or full name) list, e.g.: [12,13], [21,22], [33,34] and
-                input or select a mesh
+    :param edges: input edges (number or full name) list, e.g.: [[12,13], [21,22], [33,34]]
+    :param mesh: input or select a mesh
+    :param nb: number of rivet wanted
     :return: the rivet transform node.
     """
 
-    if args:
+    if edges:
 
         # Getting mesh if one
-        mesh = [arg for arg in args if isinstance(arg, basestring) and '.' not in arg]
         if not mesh:
             mesh = mc.ls(sl=1, type='transform')
-        if not mesh:
-            mc.warning('Please select a mesh')
-            return
 
         # Getting edges
         edges = [arg for arg in args if arg not in mesh]
-        if not edges or len(edges) < 2:
+        if not edges or len(edges) < 2 or not edges[0] or not edges[1]:
             mc.warning('Not enough or no edges given')
             return
 
         # if edges are given as integer converting them to real edges name string
-        if type(edges[0]) is int:
+        edgesType = type(edges[0])
+        if edgesType is int:
+            if not mesh:
+                mc.warning('Please select a mesh')
+                return
             edges = convertIntEdges(mesh[0], edges)
         # elif edges are given as list of integer converting them to real edges name string
-        elif type(edges[0]) is list or type(edges[0]) is tuple:
+        elif edgesType is list or edgesType is tuple:
             for i, edge in enumerate(edges):
                 if type(edge[0]) is int:
+                    if not mesh:
+                        mc.warning('Please select a mesh')
+                        return
                     edges[i] = convertIntEdges(mesh[0], edge)
+
 
     else:
         edges = [edge for edge in mc.ls(sl=True, fl=True) if '.' in edge]
@@ -123,7 +118,11 @@ def do_fRivet(*args):
     for crv in crvs:
         mc.rebuildCurve(crv, ch=True, rpo=True, rt=0, end=1, kr=0, kcp=False, kep=True, kt=False, s=0, d=3, tol=0.01)
     surf = mc.loft(crvs, ch=True, u=True, c=False, ar=True, d=3, ss=1, rn=False, po=0, rsn=False, n='rvt_surf#')[0]
-    rvt = f.do_follicle(surface=surf)[0]
+
+    # todo : to finish
+    for i in range(nb):
+        rvt = do_follicle(surface=surf, pos="""todo""")
+    # todo : to finish
 
     # grpWorld with curves and surface to clean
     grpW = mc.group(em=True, n='rvtWorld#')
@@ -173,6 +172,38 @@ def convertIntEdges(mesh, edges):
         edges[i] = mesh + '.e[' + str(edge) + ']'
     return edges
 
+
+def do_follicle(param='U', surface=None, pos=0.5):
+
+    if not surface:
+        surface = mc.ls(sl=True, fl=True)
+    if not surface:
+        mc.warning('Select a nurbs surface')
+        return
+    if not param == 'U':
+        paramlist = ['V', 'U']
+    else:
+        paramlist = ['U', 'V']
+
+    surfaceshape = getShape(surface)[0]
+    if not mc.nodeType(surfaceshape) == 'nurbsSurface':
+        mc.warning('No nurbs surface given')
+        return
+    follicleshape = mc.createNode('follicle', n=surface+'_follicleShape#')
+    follicle = mc.listRelatives(follicleshape, p=True)[0]
+    follicle = mc.rename(follicle, surface+'_follicle', ignoreShape=True)
+    mc.connectAttr(surfaceshape+'.local', follicleshape+'.inputSurface', f=True)
+    mc.connectAttr(surfaceshape+'.worldMatrix[0]', follicleshape+'.inputWorldMatrix', f=True)
+    mc.connectAttr(follicleshape+'.outRotate', follicle+'.rotate', f=True)
+    mc.connectAttr(follicleshape+'.outTranslate', follicle+'.translate', f=True)
+    for manip in 'tr':
+        for axis in 'xyz':
+            mc.setAttr(follicle+'.'+manip+axis, lock=True)
+    mc.setAttr(follicleshape+'.parameter'+paramlist[0], pos)
+    mc.setAttr(follicleshape+'.parameter'+paramlist[1], 0.5)
+
+    return follicle
+
 """
 # WIP
 def edgesSort(edges=None):
@@ -180,7 +211,7 @@ def edgesSort(edges=None):
     sort = []
     poly = edges[0].split('.')[0]
     edgesNum = [int(a.split('[')[-1].split(']')[0]) for a in edges]
-    print(edgesNum)
+    print(edgesNum)in
 
     for i, edge in enumerate(edgesNum):
         if i+1 == len(edgesNum):
