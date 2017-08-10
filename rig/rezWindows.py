428e9ef224c074f4906f11787dd1b9fab925c342
Robin Lavigne <robinservoz@gmail.com>
2017-08-10 22:18:10 +0200

    update

--- rig/rezWindows.py
+++ rig/rezWindows.py
@@ -4,4 +4,4 @@ for _w in mc.lsUI(windows=1):
     if _w not in do_not_remove:
         mc.deleteUI(_w)
         mc.windowPref(_w, remove=1)
-        print '# cleanup ui ', _w
+        print('# cleanup ui ', _w)
