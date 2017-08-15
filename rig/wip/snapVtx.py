c435056124d1eb76a7169118d7d54359b1851327
Robin Lavigne <robinservoz@gmail.com>
2017-08-16 00:09:57 +0200

    update

--- rig/wip/snapVtx.py
+++ rig/wip/snapVtx.py
@@ -9,3 +9,32 @@ def do_snapVtx(objs=None, os=False, closest=False):
     if len(objs) < 2:
         mc.warning("Select at least two mesh")
         return
+
+    master = objs[-1]
+    slaves = objs[:-1]
+
+    mc.select(mc.polyListComponentConversion(master, tv=True))
+    master_vtx = mc.ls(sl=True, fl=True)
+    if closest or not os:
+        master_vtx_pos = [mc.xform(x, q=True, ws=True, t=True) for x in master_vtx]
+    else:
+        master_vtx_pos = [mc.xform(x, q=True, os=True, t=True) for x in master_vtx]
+
+    for slave in slaves:
+        mc.select(mc.polyListComponentConversion(slave, tv=True))
+        slave_vtx = mc.ls(sl=True, fl=True)
+        if closest or not os:
+            slave_vtx_pos = [mc.xform(x, q=True, ws=True, t=True) for x in slave_vtx]
+        else:
+            slave_vtx_pos = [mc.xform(x, q=True, os=True, t=True) for x in slave_vtx]
+
+        if not closest:
+            for i, vtx in enumerate(slave_vtx):
+                if not os:
+                    mc.xform(vtx, ws=True, t=master_vtx_pos[i])
+                else:
+                    mc.xform(vtx, os=True, t=master_vtx_pos[i])
+
+        else:
+
+
