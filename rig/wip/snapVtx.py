d216b6f42693d59ffe3595bbe450fd050ca772b3
laro <robinservoz@gmail.com>
2017-08-16 19:16:18 +0200

    update

--- rig/wip/snapVtx.py
+++ rig/wip/snapVtx.py
@@ -1,6 +1,7 @@
 import maya.cmds as mc
 from tbx import get_shape
 
+
 def do_snapVtx(objs=None, os=False, closest=False):
 
     if not objs:
@@ -10,8 +11,8 @@ def do_snapVtx(objs=None, os=False, closest=False):
         mc.warning("Select at least two mesh")
         return
 
-    master = objs[-1]
-    slaves = objs[:-1]
+    master = objs[0]
+    slaves = objs[1:]
 
     mc.select(mc.polyListComponentConversion(master, tv=True))
     master_vtx = mc.ls(sl=True, fl=True)
@@ -23,10 +24,6 @@ def do_snapVtx(objs=None, os=False, closest=False):
     for slave in slaves:
         mc.select(mc.polyListComponentConversion(slave, tv=True))
         slave_vtx = mc.ls(sl=True, fl=True)
-        if closest or not os:
-            slave_vtx_pos = [mc.xform(x, q=True, ws=True, t=True) for x in slave_vtx]
-        else:
-            slave_vtx_pos = [mc.xform(x, q=True, os=True, t=True) for x in slave_vtx]
 
         if not closest:
             for i, vtx in enumerate(slave_vtx):
@@ -34,7 +31,6 @@ def do_snapVtx(objs=None, os=False, closest=False):
                     mc.xform(vtx, ws=True, t=master_vtx_pos[i])
                 else:
                     mc.xform(vtx, os=True, t=master_vtx_pos[i])
-
         else:
-
+            slave_vtx_pos = [mc.xform(x, q=True, ws=True, t=True) for x in slave_vtx]
 
