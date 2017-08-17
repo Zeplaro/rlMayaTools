dcc5158b6a4e22f27d8f5b7ccf1602b46ccd2d73
laro <robinservoz@gmail.com>
2017-08-17 19:14:33 +0200

    update

--- rig/wip/snapVtx.py
+++ rig/wip/snapVtx.py
@@ -1,11 +1,19 @@
 import maya.cmds as mc
 from tbx import get_shape
+from math import sqrt
 
+# todo : do closest if sel is vtx
+
+
+def get_dist(a, b):
+    dist = sqrt(pow((b[0]-a[0]), 2)+pow((b[1]-a[1]), 2)+pow((b[2]-a[2]), 2))
+    return dist
 
 def do_snapVtx(objs=None, os=False, closest=False):
 
+    sel = mc.ls(sl=True)
     if not objs:
-        objs = [x for x in mc.ls(sl=True) if 'mesh' in mc.nodeType(get_shape(x), i=True) and '.' not in x]
+        objs = [x for x in sel if 'mesh' in (mc.nodeType(get_shape(x), i=True) or []) and '.' not in x]
     objs = [x for x in list(objs) if mc.objExists(x)]
     if len(objs) < 2:
         mc.warning("Select at least two mesh")
@@ -33,4 +41,20 @@ def do_snapVtx(objs=None, os=False, closest=False):
                     mc.xform(vtx, os=True, t=master_vtx_pos[i])
         else:
             slave_vtx_pos = [mc.xform(x, q=True, ws=True, t=True) for x in slave_vtx]
+            for i, vtx in enumerate(slave_vtx):
+                print vtx
+                dists = []
+                for j in master_vtx_pos:
+                    dist = get_dist(slave_vtx_pos[i], j)
+                    dists.append(dist)
+                index = 0
+                val = dists[0]
+                for k_, dist in enumerate(dists):
+                    if val < dist:
+                        index = k_
+                        val = dist
+                    else:
+                        continue
+                mc.xform(vtx, ws=True, t=master_vtx_pos[index])
+    mc.select(sel)
 
