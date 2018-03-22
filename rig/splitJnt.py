1a10ffe8c658637af9acd8f6d649c6c076e5391f
Robin Lavigne <robinservoz@gmail.com>
2018-03-22 16:39:37 -0700

    add ui

--- rig/splitJnt.py
+++ rig/splitJnt.py
@@ -1,8 +1,60 @@
 import maya.cmds as mc
-# todo : ui
+from functools import partial
 
 
-def do_splitJnt(nb=1, jnts=None):
+def launch_ui():
+
+    if mc.window('splitJntUI', exists=True):
+        mc.deleteUI('splitJntUI')
+    splitJnt_ui()
+
+
+def splitJnt_ui():
+
+        mc.window('splitJntUI', title='Split joint tool', s=False, rtf=True)
+
+        mc.columnLayout('columnMain', w=400)
+        mc.rowLayout('startRow', nc=3)
+        mc.text('Start joint :', w=90)
+        mc.textField('startJnt', w=250)
+        mc.button('startUpdate', l='Update', command=partial(getSel, 'startJnt'))
+        mc.setParent('..')
+        mc.rowLayout('endRow', nc=3)
+        mc.text('End joint :', w=90)
+        mc.textField('endJnt', w=250)
+        mc.button('endUpdate', l='Update', command=partial(getSel, 'endJnt'))
+        mc.setParent('..')
+        mc.intSliderGrp('nb', l='Number of split', cw=(1, 90), cl3=('center', 'center', 'center'), value=1, min=1,
+                        max=10, field=True, fieldMinValue=1, fieldMaxValue=1000, w=400)
+        mc.rowLayout('suffixRow', nc=2)
+        mc.text('Split suffix :', w=90)
+        mc.textField('suffix', text='_split_', w=250)
+        mc.setParent('..')
+        mc.button('Split', w=400, command=launch_splitJnt)
+        mc.showWindow('splitJntUI')
+
+
+def getSel(btn, *args):
+
+    sel = mc.ls(sl=True, type='joint')
+    if not sel:
+        mc.warning('No joint selected')
+        return
+    mc.textField(btn, e=True, text=sel[0])
+
+
+def launch_splitJnt(*args):
+
+        nb = mc.intSliderGrp('nb', q=True, value=True)
+        jnts = [mc.textField('startJnt', q=True, text=True), mc.textField('endJnt', q=True, text=True)]
+        suffix = mc.textField('suffix', q=True, text=True)
+        if jnts[0] and jnts[1]:
+            do_splitJnt(nb, jnts, suffix)
+        else:
+            do_splitJnt(nb=nb, jnts=None, suffix=suffix)
+
+
+def do_splitJnt(nb=1, jnts=None, suffix='_split_'):
 
     if not jnts:
         jnts = mc.ls(sl=True, fl=True, type='joint')
@@ -27,7 +79,7 @@ def do_splitJnt(nb=1, jnts=None):
     pos = splitpos[:]
     for i in range(nb):
         mc.select(cl=True)
-        split = mc.joint(n=start+'_split_#', rad=rad)
+        split = mc.joint(n=start+suffix+'#', rad=rad)
         mc.parent(split, start, r=True)
         mc.xform(split, r=True, t=pos)
         for j in range(3):
