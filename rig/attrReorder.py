8c9a0bf80e6578b109beb7cbf320a970d2ffd52b
Robin Lavigne <robinservoz@gmail.com>
2018-03-26 01:03:17 -0700

    new attributeReorderer

---
+++ rig/attrReorder.py
@@ -0,0 +1,94 @@
+import maya.cmds as mc
+from functools import partial
+
+
+def launch_ui():
+
+    if mc.window('attrReoderUI', exists=True):
+        mc.deleteUI('attrReoderUI')
+    attrReorder()
+
+
+class attrReorder():
+
+    obj = None
+    attrs = []
+
+    def __init__(self):
+        self.main_ui()
+        mc.showWindow('attrReoderUI')
+
+    def main_ui(self):
+
+        mc.window('attrReoderUI', title='Attribute reorderer', s=True, rtf=True)
+        mc.columnLayout()
+        mc.button('reload', l='Reload selected', w=100, command=self.reload_sel)
+        mc.rowLayout('rowMain', nc=2)
+        mc.columnLayout('columnUpDown')
+        mc.button('up', l='^', w=25, command=partial(self.move, 'up'))
+        mc.button('down', l='v', w=25, command=partial(self.move, 'down'))
+        mc.setParent('..')
+        mc.columnLayout('columnAttrs')
+        self.radioButton_ui()
+
+    def radioButton_ui(self, sel=None):
+        mc.columnLayout('radioButtons', p='columnAttrs')
+        mc.radioCollection('attrs')
+
+        self.obj = self.get_obj()
+        if not self.obj:
+            mc.warning('Select an object')
+            return
+        self.attrs = self.get_attrs()
+        if not self.attrs:
+            mc.warning('No reorderable attributes on selection')
+            return
+
+        for attr in self.attrs:
+            if sel == attr:
+                mc.radioButton(attr, sl=True)
+            else:
+                mc.radioButton(attr)
+
+    def get_obj(self):
+        sel = mc.ls(sl=True, fl=True)
+        if not sel:
+            return None
+        else:
+            sel = sel[0]
+        return sel
+
+    def get_attrs(self):
+        attributes = mc.listAttr(self.obj, ud=True, visible=True)
+        return attributes
+
+    def reload_sel(self, sel=None, *args):
+        mc.deleteUI('radioButtons')
+        self.radioButton_ui(sel=sel)
+
+    def move(self, way, *args):
+        attr = mc.radioCollection('attrs', q=True, sl=True)
+        index = self.attrs.index(attr)
+        if way == 'up':
+            if index > 0:
+                newIndex = index-1
+            else:
+                return
+        else:
+            newIndex = index+1
+        self.attrs.pop(index)
+        self.attrs.insert(newIndex, attr)
+
+        for attribute in self.attrs:
+            if mc.getAttr(self.obj+'.'+attribute, lock=True):
+                lock = True
+                mc.setAttr(self.obj+'.'+attribute, lock=False)
+            else:
+                lock = False
+            mc.deleteAttr(self.obj+'.'+attribute)
+            mc.undo()
+            if lock:
+                mc.setAttr(self.obj+'.'+attribute, lock=True)
+
+        self.reload_sel(attr)
+        return
