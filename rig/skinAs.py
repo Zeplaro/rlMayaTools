b8d8e748168bb76f9d69d4d04356903dc71cf98a
Robin Lavigne <robinservoz@gmail.com>
2018-04-22 23:36:16 -0700

    add slaveNamespace possibility

--- rig/skinAs.py
+++ rig/skinAs.py
@@ -2,7 +2,7 @@ import maya.cmds as mc
 from tbx import get_skinCluster
 
 
-def do_skinAs(master=None, *slaves):
+def do_skinAs(slaveNamespace=None, master=None, *slaves):
 
     if not master or not slaves:
         objs = mc.ls(sl=True, tr=True, fl=True)
@@ -16,7 +16,9 @@ def do_skinAs(master=None, *slaves):
     if not masterskn:
         mc.warning('First object as no skinCluster attached')
         return
-    inf = mc.skinCluster(masterskn, q=True, inf=True)
+    infs = mc.skinCluster(masterskn, q=True, inf=True)
+    if slaveNamespace is not None:
+        infs = ['{}{}'.format(slaveNamespace, inf) for inf in infs]
     sm = mc.skinCluster(masterskn, q=True, sm=True)
     mi = mc.skinCluster(masterskn, q=True, mi=True)
     nw = mc.skinCluster(masterskn, q=True, nw=True)
@@ -27,8 +29,8 @@ def do_skinAs(master=None, *slaves):
         if get_skinCluster(slave):
             print(slave+' already have a skin attached')
             continue
-        mc.select(inf, slave, r=True)
-        mc.skinCluster(name='skinCluster_'+slave+'_#', sm=sm, mi=mi, nw=nw, omi=omi, wd=wd, ihs=True, tsb=True)
+        mc.select(infs, slave, r=True)
+        mc.skinCluster(name='skinCluster_{}_#'.format(slave), sm=sm, mi=mi, nw=nw, omi=omi, wd=wd, ihs=True, tsb=True)
         slaveskn = get_skinCluster(slave)
         mc.copySkinWeights(ss=masterskn, ds=slaveskn, nm=True, sa='closestPoint', ia=('oneToOne', 'label', 'closestJoint'))
         print(slave+' skinned')
