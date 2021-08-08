9289507ab519c8fbc1aeb96170843136aa2271b7
robin <robinservoz@gmail.com>
2021-08-08 16:18:50 -0700

    added comments and NurbsCurve class

--- rig/yama/nodes/__init__.py
+++ rig/yama/nodes/__init__.py
@@ -2,9 +2,16 @@ import maya.cmds as mc
 import importlib
 import depend as dpn
 
+
+"""
+Lists the supported types of maya nodes. Any new node class should be added to this list to be able to get it from the 
+node_to_class method.
+When adding a new class follow this syntax ->   'mayaType': ('fileContainingTheClass', 'ClassName')
+"""
 suported_class = {'skinCluster': ('skinCluster', 'SkinCluster'),
                   'shape': ('shape', 'Shape'),
                   'mesh': ('shape', 'Mesh'),
+                  'nurbsCurve': ('shape', 'NurbsCurve'),
                   'geometryFilter': ('geometryFilter', 'GeometryFilter'),
                   'weightGeometryFilter': ('geometryFilter', 'WeightGeometryFilter'),
                   'blendShape': ('blendShape', 'BlendShape'),
@@ -16,7 +23,7 @@ suported_class = {'skinCluster': ('skinCluster', 'SkinCluster'),
 def node_to_class(node):
     for node_type in mc.nodeType(node, i=True)[::-1]:
         if node_type in suported_class:
-            lib = importlib.import_module('deformer.' + suported_class[node_type][0])
+            lib = importlib.import_module(suported_class[node_type][0])
             return lib.__getattribute__(suported_class[node_type][1])(node)
     return dpn.Depend(node)
 
