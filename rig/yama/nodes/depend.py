cedcc56c8ac6b1c0d9dbeb77cba2e342bc77a238
robin <robinservoz@gmail.com>
2021-08-20 15:00:32 -0700

    Moved node to class from init to depend. Added listRelatives

--- rig/yama/nodes/depend.py
+++ rig/yama/nodes/depend.py
@@ -1,5 +1,38 @@
+import importlib
 import maya.cmds as mc
 import attribute as attr
+
+
+"""
+Lists the supported types of maya nodes. Any new node class should be added to this list to be able to get it from the 
+node_to_class method.
+When adding a new class follow this syntax ->   'mayaType': ('fileContainingTheClass', 'ClassName')
+"""
+suported_class = {'skinCluster': ('skinCluster', 'SkinCluster'),
+                  'shape': ('shape', 'Shape'),
+                  'mesh': ('shape', 'Mesh'),
+                  'nurbsCurve': ('shape', 'NurbsCurve'),
+                  'geometryFilter': ('geometryFilter', 'GeometryFilter'),
+                  'weightGeometryFilter': ('geometryFilter', 'WeightGeometryFilter'),
+                  'blendShape': ('blendShape', 'BlendShape'),
+                  'transform': ('transform', 'Transform'),
+                  'joint': ('transform', 'Joint'),
+                  }
+
+
+def node_to_class(node):
+    for node_type in mc.nodeType(node, i=True)[::-1]:
+        if node_type in suported_class:
+            lib = importlib.import_module(suported_class[node_type][0])
+            return lib.__getattribute__(suported_class[node_type][1])(node)
+    return Depend(node)
+
+
+ntc = node_to_class
+
+
+def nodes_to_classes(nodes):
+    return [node_to_class(node) for node in nodes]
 
 
 class Depend(object):
@@ -12,7 +45,7 @@ class Depend(object):
         self._type_inheritance = mc.nodeType(name, i=True)
 
     def __repr__(self):
-        return "yama.{}('{}')".format(self.__class__.__name__, self._name)
+        return "{}('{}')".format(self.__class__.__name__, self._name)
 
     def __str__(self):
         return self._name
@@ -35,3 +68,6 @@ class Depend(object):
     @property
     def name(self):
         return self._name
+
+    def listRelatives(self, *args, **kwargs):
+        return nodes_to_classes(mc.listRelatives(self, *args, **kwargs) or [])
