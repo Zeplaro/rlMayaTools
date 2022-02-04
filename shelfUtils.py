9e5087ae92d8a824dbbc2fab82a0b9951b2e4998
robin <robinservoz@gmail.com>
2022-02-04 10:31:35 -0800

    Added docstrings

--- shelfUtils.py
+++ shelfUtils.py
@@ -2,10 +2,17 @@ from maya import cmds, mel
 
 
 def get_current_shelf():
+    """Gets the current shelf name"""
     return mel.eval("tabLayout -q -selectTab $gShelfTopLevel")
 
 
 def get_shelf_data(shelf=None):
+    """
+    Gets the given shelf data into a list of dict. To be easily restored using set_shelf_data.
+    Instead of using the maya generated shelf file in mel that does not always work between different maya versions.
+    :param shelf: str of an existing shelf name
+    :return: [dict, dict, ...] list of dict data per shelf item
+    """
     button_kwargs = ('imageOverlayLabel', 'command', 'sourceType', 'image', 'annotation', 'doubleClickCommand', 'label')
     menu_kwargs = ('label', 'command', 'sourceType')
 
@@ -24,12 +31,19 @@ def get_shelf_data(shelf=None):
             for menu in cmds.shelfButton(button, q=True, pma=True) or []:
                 for item in cmds.popupMenu(menu, q=True, itemArray=True) or []:
                     button_data['submenus'].append(item)
-                    button_data['submenus_data'][item] = {kwarg: cmds.menuItem(item, q=True, **{kwarg: True}) for kwarg in menu_kwargs}
+                    button_data['submenus_data'][item] = {kwarg: cmds.menuItem(item, q=True, **{kwarg: True})
+                                                          for kwarg in menu_kwargs}
         data.append(button_data)
     return data
 
 
 def set_shelf_data(data, shelf=None):
+    """
+    Sets the given shelf data from get_shelf_data.
+    Instead of using the maya generated shelf file in mel that does not always work between different maya versions.
+    :param data: [dict, dict, ...] list of dict data per shelf item
+    :param shelf: str of an existing shelf name
+    """
     if not shelf:
         shelf = get_current_shelf()
     for button in data:
