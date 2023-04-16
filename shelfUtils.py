9726e3a1995561c0b033db8d24f7a57b73db0975
robin <robinservoz@gmail.com>
2023-04-15 22:56:06 -0700

    Improved and fixed

--- shelfUtils.py
+++ shelfUtils.py
@@ -1,4 +1,5 @@
 from maya import cmds, mel
+from copy import deepcopy
 
 
 def get_current_shelf():
@@ -15,25 +16,25 @@ def get_shelf_data(shelf=None):
     """
     button_kwargs = ('imageOverlayLabel', 'command', 'sourceType', 'image', 'annotation', 'doubleClickCommand', 'label')
     menu_kwargs = ('label', 'command', 'sourceType')
+    excluded_labels = ('Open', 'Edit', 'Edit Popup', 'Delete')
 
     if not shelf:
         shelf = get_current_shelf()
     data = []
     for button in cmds.shelfLayout(shelf, query=True, childArray=True) or []:
-        uiType = cmds.objectTypeUI(button)
-        button_data = {}
-        if uiType == 'separator':
-            button_data = 'separator'
-        if uiType == 'shelfButton':
+        ui_type = cmds.objectTypeUI(button)
+        if ui_type == 'separator':
+            data.append('separator')
+        elif ui_type == 'shelfButton':
             button_data = {kwarg: cmds.shelfButton(button, q=True, **{kwarg: True}) for kwarg in button_kwargs}
-            button_data['submenus'] = []
-            button_data['submenus_data'] = {}
+            button_data['menu'] = []
             for menu in cmds.shelfButton(button, q=True, pma=True) or []:
                 for item in cmds.popupMenu(menu, q=True, itemArray=True) or []:
-                    button_data['submenus'].append(item)
-                    button_data['submenus_data'][item] = {kwarg: cmds.menuItem(item, q=True, **{kwarg: True}) or ''
-                                                          for kwarg in menu_kwargs}
-        data.append(button_data)
+                    if cmds.menuItem(item, q=True, label=True) in excluded_labels:
+                        continue
+                    menu_data = {kwarg: cmds.menuItem(item, q=True, **{kwarg: True}) or '' for kwarg in menu_kwargs}
+                    button_data['menu'].append(menu_data)
+            data.append(button_data)
     return data
 
 
@@ -46,13 +47,12 @@ def set_shelf_data(data, shelf=None):
     """
     if not shelf:
         shelf = get_current_shelf()
-    for button in data:
-        if button == 'separator':
+    for button_data in deepcopy(data):  # deepcopy to not alter the given data object or its values with later .pop()
+        if button_data == 'separator':
             cmds.separator(width=12, height=35, style='shelf', hr=False, parent=shelf)
         else:
-            submenus = button.pop('submenus')
-            submenus_data = button.pop('submenus_data')
-            button = cmds.shelfButton(parent=shelf, **button)
+            menu_items = button_data.pop('menu')
+            button = cmds.shelfButton(parent=shelf, **button_data)
             menu = cmds.shelfButton(button, q=True, pma=True)[0]
-            for item in submenus:
-                cmds.menuItem(parent=menu, **submenus_data[item])
+            for menu_data in menu_items:
+                cmds.menuItem(parent=menu, **menu_data)
