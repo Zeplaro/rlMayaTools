dd61ebcc71a25ffef42fa7f0f42274488e9c12c6
robin <robinservoz@gmail.com>
2025-04-29 11:31:39 -0700

    UI code

--- rig/shapes/ui.py
+++ rig/shapes/ui.py
@@ -1,212 +1,87 @@
 # encoding: utf8
 
-import numpy as np
+import os
+import json
+from pathlib import Path
+from functools import partial
+from importlib import reload
+
+import maya.OpenMayaUI as omui
 from maya import cmds
 
-import yama as ym
+from Qt import QtGui, QtCore, QtWidgets, QtCompat
+
+from shapes import shapes
+reload(shapes)
 
 
-def shapesFromSelection():
-    shapes = ym.YamList()
-    for i in ym.selected():
-        if i.isa("transform"):
-            shapes += i.shapes(type="nurbsCurve")
-        elif i.isa("nurbsCurve"):
-            shapes.append(i)
-    return shapes
+TITLE = "rl Shapes"
+BASE_PATH = Path(__file__).parent
 
 
-class Mirror:
-    @staticmethod
-    def matrix_maya_to_row(matrix):
-        matrix = [round(x, 3) for x in matrix]
-        return [matrix[0:4], matrix[4:8], matrix[8:12], matrix[12:16]]
-
-    @staticmethod
-    def compare_to_world(matrix):
-        if len(matrix) > 3:
-            matrix.pop(-1)
-            for row in matrix:
-                row.pop(-1)
-        world = [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]
-        result = np.matmul(world, matrix)
-        table = []
-        for row in result:
-            max_value = max(row, key=abs)
-            axis = "xyz"[list(row).index(max_value)]
-            if max_value < 0:
-                axis = "-" + axis
-            table.append(axis)
-        return table
-
-    @classmethod
-    def get_mirror_table(cls, left, right, axis="x"):
-        left = cls.compare_to_world(
-            cls.matrix_maya_to_row(left.getXform(m=True, ws=True))
-        )
-        right = cls.compare_to_world(
-            cls.matrix_maya_to_row(right.getXform(m=True, ws=True))
-        )
-        table = []
-        for l_axis, r_axis in zip(left, right):
-            if l_axis == r_axis:
-                value = 1
-            else:
-                value = -1
-            if l_axis[-1] == axis:
-                value *= -1
-            table.append(value)
-        return table
-
-    @classmethod
-    def mirror(cls, left, axis="x"):
-        left = ym.yam(left)
-        right = cls.findMirror(left)
-        if not right:
-            return
-        table = cls.get_mirror_table(left, right, axis=axis)
-        for l_shape, r_shape in zip(left.shapes(), right.shapes()):
-            for pose, r_cv in zip(l_shape.cv.getPositions(), r_shape.cv):
-                new_pose = [x * mult for x, mult in zip(pose, table)]
-                r_cv.setPosition(new_pose, ws=False)
-
-    @staticmethod
-    def findMirror(ctrl):
-        mirror_ctrl = ""
-        if ctrl.name.startswith("l_"):
-            mirror_ctrl = "r_" + ctrl.name[2:]
-        elif ctrl.name.startswith("r_"):
-            mirror_ctrl = "l_" + ctrl.name[2:]
-        elif ":" in ctrl.name:
-            split = ctrl.name.split(":")
-            if split[-1].startswith("l_"):
-                split[-1] = "r_" + split[-1][2:]
-                mirror_ctrl = ":".join(split)
-            elif split[-1].startswith("r_"):
-                split[-1] = "r_" + split[-1][2:]
-                mirror_ctrl = ":".join(split)
-
-        if not cmds.objExists(mirror_ctrl):
-            print(f"No mirror ctrl of '{ctrl}' found")
-            return
-        return ym.yam(mirror_ctrl)
-
-    @classmethod
-    def mirror_selected(cls):
-        sel = ym.selected()
-        print(f"Mirroring {len(sel)} objects")
-        for i in sel:
-            cls.mirror(i)
-        print("Done")
+def get_maya_window():
+    """Getting maya's window"""
+    ptr = omui.MQtUtil.mainWindow()
+    parent = QtCompat.wrapInstance(int(ptr), QtWidgets.QMainWindow)
+    return parent
 
 
-def copyShapes(ws=False):
-    source, *targets = ym.selected()
-    if source.isa("transform"):
-        source = source.shapes(type="nurbsCurve")
-    elif source.isa("nurbsCurve"):
-        source = [source]
-    else:
-        return
-
-    target_shapes = []
-    for target in targets:
-        if target.isa("nurbsCurve"):
-            target_shapes.append([target])
-        elif target.isa("transform"):
-            target_shapes.append(target.shapes(type="nurbsCurve"))
-
-    source_positions = [shape.cv.getPositions(ws=ws) for shape in source]
-    for target in target_shapes:
-        for source_pos, target_shape in zip(source_positions, target):
-            target_shape.cv.setPositions(source_pos, ws=ws)
+def close_existing(target_title):
+    parent = get_maya_window()
+    children = parent.children()
+    for child in children:
+        try:
+            title = child.windowTitle()
+        except AttributeError:
+            title = ""
+        if title == target_title:
+            try:
+                child.close()
+            except ValueError:
+                print(f"failed to close '{target_title}'")
 
 
-def selectShapes():
-    shapes = shapesFromSelection()
-    ym.select(shapes)
-    print(f"Selected : {shapes.names}")
+def center_ui(ui):
+    # Get the cursor position
+    cursor_pos = QtGui.QCursor.pos()
+    # Get the screen the cursor is on
+    screen = QtWidgets.QApplication.screenAt(cursor_pos)
+    # Get the center point of the screen
+    screen_center = screen.geometry().center()
+    # Get the center point of the UI
+    ui_center = ui.frameGeometry().center()
+    # Calculate the position to move the UI to the center of the screen
+    center_pos = screen_center - ui_center
+    # Move the UI to the calculated position
+    ui.move(center_pos)
 
 
-def selectIntermediateShapes():
-    shapes = ym.YamList(x.intermediateShapes(type="nurbsCurve") for x in ym.selected() if x.isa("transform"))
-    ym.select(*shapes)
-    print(f"Selected : {shapes}")
+def launch_ui():
+    close_existing(TITLE)
+    ui = MainUI(parent=get_maya_window(), title=TITLE)
+    ui.show()
+    center_ui(ui.window())
+    return ui
 
 
-def selectCvs():
-    cvs = [shape.cv for shape in shapesFromSelection()]
-    ym.select(*cvs)
+class MainUI(QtWidgets.QMainWindow):
+    def __init__(self, parent=None, title=TITLE):
+        super().__init__(parent=parent)
+        # Loading the .ui file
+        self.ui = QtCompat.loadUi(str(BASE_PATH / "shapes.ui"), self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
+        self.setWindowTitle(title)
 
+        # UI LAYOUT
 
-def parentShapes():
-    cmds.parent(r=True, s=True)
+        self.ui_connections()
 
+    def ui_connections(self):
+        for index in range(32):
+            button = getattr(self, f"color{index}Button")
+            button.clicked.connect(partial(shapes.indexColor, index, True))
+        self.copyShapeButton.clicked.connect(self.copy)
+        self.mirrorOtherSideButton.clicked.connect(shapes.Mirror.mirror_selected)
 
-def indexColor(index, on_transform):
-    objs = set()
-    if on_transform:
-        for i in ym.selected():
-            if i.isa("transform"):
-                objs.add(i)
-            elif i.isa("shape"):
-                objs.add(i.parent)
-    else:
-        objs = shapesFromSelection()
-
-    for obj in objs:
-        obj.overrideEnabled.value = True
-        obj.overrideRGBColors.value = False
-        obj.overrideColor.value = index
-
-
-def rgbColor():
-    ...
-
-
-def copyColor(on_transform):
-    source, *objs = ym.selected()
-
-    if source.isa("transform"):
-        source = source.shape
-    while True:
-        if source.overrideEnabled.value:
-            break
-        if not source.parent:
-            source = None
-            break
-        source = source.parent
-
-    targets = []
-    if on_transform:
-        for i in objs:
-            if i.isa("transform"):
-                targets.add(i)
-            elif i.isa("shape"):
-                targets.add(i.parent)
-    else:
-        targets = shapesFromSelection()
-
-    if not source:
-        for target in targets:
-            while target:
-                target.overrideEnabled.value = False
-                target = target.parent
-
-    else:
-        rgb = source.overrideRGBColors.value
-        index = source.overrideColor.value
-        rgb_color = source.overrideColorRGB.value
-        for target in targets:
-            target.overrideEnabled.value = True
-            target.overrideRGBColors.value = rgb
-            target.overrideColor.value = index
-            target.overrideColorRGB.value = rgb_color
-
-
-def setDrawingOverride(value):
-    for i in ym.selected():
-        i.overrideEnabled.value = value
-
-
-
+    def copy(self):
+        shapes.copyShapes(self.copyShapeWorldRadioButton.isChecked())
